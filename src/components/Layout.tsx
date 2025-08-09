62b8f74708bf0a23241f4691213bfb7c8dfae967
[dyad] <git@dyad.sh>
2025-08-10 00:46:15 +0530

    [dyad] Added a tool switcher to the bottom of each page - wrote 2 file(s)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Header } from './Header';
 import { MadeWithDyad } from './made-with-dyad';
+import { ToolSwitcher } from './ToolSwitcher';
 
 export const Layout = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+
   return (
     <div className="relative flex min-h-screen flex-col bg-gradient-to-br from-sky-100 to-rose-100 dark:from-slate-900 dark:to-rose-950">
       <Header />
-      <main className="flex flex-1">
+      <main className="flex flex-1 flex-col">
         <Outlet />
+        {!isHomePage && <ToolSwitcher />}
       </main>
       <div className="border-t bg-transparent">
         <MadeWithDyad />

25676b41b30601f91fb211f1e60308fa609ed6da
[dyad] <git@dyad.sh>
2025-08-10 16:49:31 +0530

    [dyad] Fixing CORS by reverting Deno version - wrote 4 file(s)

--- supabase/functions/create-razorpay-order/index.ts
+++ supabase/functions/create-razorpay-order/index.ts
@@ -1,6 +1,6 @@
-import { serve } from "https://deno.land/std@0.224.0/http/server.ts"
+import { serve } from "https://deno.land/std@0.192.0/http/server.ts"
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.45.0'
-import { encode } from "https://deno.land/std@0.224.0/encoding/base64.ts";
+import { encode } from "https://deno.land/std@0.192.0/encoding/base64.ts";
 
 console.log("Function cold start: create-razorpay-order");
 
--- supabase/functions/verify-razorpay-payment/index.ts
+++ supabase/functions/verify-razorpay-payment/index.ts
@@ -1,7 +1,7 @@
-import { serve } from "https://deno.land/std@0.224.0/http/server.ts"
+import { serve } from "https://deno.land/std@0.192.0/http/server.ts"
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.45.0'
-import { timingSafeEqual } from "https://deno.land/std@0.224.0/crypto/timing_safe_equal.ts";
-import { encodeToString } from "https://deno.land/std@0.224.0/encoding/hex.ts";
+import { timingSafeEqual } from "https://deno.land/std@0.192.0/crypto/timing_safe_equal.ts";
+import { encodeToString } from "https://deno.land/std@0.192.0/encoding/hex.ts";
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
